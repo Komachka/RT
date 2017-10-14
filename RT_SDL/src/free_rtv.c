15160293607573ba1b641b0fd97bf83f8d28d152
pkolomiy <pkolomiy@student.unit.ua>
2017-10-14 05:05:55 +0300

    free rtv function

---
+++ RT_SDL/src/free_rtv.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   free_rtv.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pkolomiy <pkolomiy@student.unit.ua>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/10/14 04:09:00 by pkolomiy          #+#    #+#             */
+/*   Updated: 2017/10/14 04:09:00 by pkolomiy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "rtv.h"
+
+void	free_rtv(t_rtv *rtv)
+{
+	t_plane_with_hole	*pl;
+	t_polygon			*p;
+
+	while (--rtv->figure_num != -1)
+	{
+		if (rtv->objects[rtv->figure_num].id == PLANE_WITH_HOLE)
+		{
+			pl = (t_plane_with_hole*)rtv->objects[rtv->figure_num].object;
+			free(pl->hole);
+		}
+		else if (rtv->objects[rtv->figure_num].id == POLYGON)
+		{
+			p = (t_polygon*)rtv->objects[rtv->figure_num].object;
+			free(p->vx);
+			free(p->index);
+			free(p->col);
+			free(p->norm);
+		}
+		free(rtv->objects[rtv->figure_num].object);
+		if (rtv->objects[rtv->figure_num].texturing == ON)
+			free(rtv->objects[rtv->figure_num].texture.tx_struct);
+	}
+	free(rtv->objects);
+	free(rtv->l);
+}
